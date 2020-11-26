dd58387a5f78bdf3a2ab9b022b68c4c1d4dcacf6
Michael Gold <mgold@spscommerce.com>
2020-11-26 09:25:36 -0500

    Styling up demo 1

--- demos/ts/src/demos/Demo1.tsx
+++ demos/ts/src/demos/Demo1.tsx
@@ -1,4 +1,97 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+// | STYLING STUFF BEGINS
+const CenteredDisplay = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const Container = styled.div`
+  padding: 60px 35px 35px 35px;
+  border-radius: 40px;
+  background: #ecf0f3;
+  box-shadow: 13px 13px 20px #cbced1, -13px -13px 20px #ffffff;
+`;
+
+const TextBox = styled.input`
+  border: none;
+  outline: none;
+  background: none;
+  font-size: 18px;
+  width: 100%;
+  box-sizing: border-box;
+  color: #555;
+  padding: 20px 10px 20px 5px;
+  padding-left: 30px;
+
+  margin-bottom: 30px;
+  margin-right: 10px;
+  border-radius: 25px;
+  box-shadow: inset 8px 8px 8px #cbced1, inset -8px -8px 8px #ffffff;
+`;
+
+const Display = styled.p`
+  font-size: 30px;
+  color: #666;
+  padding: 20px;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  height: 50px;
+  border-radius: 14px;
+  background: #ecf0f3;
+  box-shadow: inset 14px 14px 20px #d0d3d6, inset -14px -14px 20px #ffffff;
+`;
+
+const CheckboxInput = styled.input`
+  position: absolute;
+  left: -9999px;
+`;
+
+const CheckboxLabel = styled.label`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #777;
+
+  padding: 10px;
+  margin-bottom: 20px;
+  user-select: none;
+  border-radius: 34px;
+  background: #ecf0f3;
+  box-shadow: 14px 14px 20px #d0d3d6, -14px -14px 20px #ffffff;
+  cursor: pointer;
+
+  ${(props: { checked: boolean }) =>
+    props.checked
+      ? "box-shadow: inset 14px 14px 20px #d0d3d6,       inset -14px -14px 20px #ffffff;"
+      : null}
+`;
+
+const Checkbox: React.FC<{
+  label: string;
+  checked: boolean;
+  onChange?: () => void;
+}> = ({ label, onChange, checked }) => {
+  return (
+    <>
+      <CheckboxLabel checked={checked}>
+        <CheckboxInput
+          type="checkbox"
+          checked={checked}
+          onChange={() => (onChange ? onChange() : null)}
+        />
+        {label}
+      </CheckboxLabel>
+    </>
+  );
+};
+
+// | STYLING STUFF ENDS
 
 const giveSpeech = (txt: string, yelling: boolean, questioning: boolean) => {
   return `${yelling ? txt.toUpperCase() : txt}${questioning ? "?" : ""}`;
@@ -9,23 +102,21 @@ export const Demo1 = () => {
   const [yelling, setYelling] = useState(false);
   const [questioning, setQuestioning] = useState(false);
   return (
-    <div>
-      <input value={txt} onChange={(e) => setTxt(e.target.value)} />
-      <label htmlFor="yelling">Yelling: </label>
-      <input
-        type="checkbox"
-        id="yelling"
-        checked={yelling}
-        onChange={() => setYelling(!yelling)}
-      />
-      <label htmlFor="questioning">Questioning:</label>
-      <input
-        type="checkbox"
-        checked={questioning}
-        id="questioning"
-        onChange={() => setQuestioning(!questioning)}
-      />
-      <p></p>
-    </div>
+    <CenteredDisplay>
+      <Container>
+        <TextBox value={txt} onChange={(e) => setTxt(e.target.value)} />
+        <Checkbox
+          label="Yelling"
+          checked={yelling}
+          onChange={() => setYelling(!yelling)}
+        />
+        <Checkbox
+          label="Questioning"
+          checked={questioning}
+          onChange={() => setQuestioning(!questioning)}
+        />
+        <Display>{txt}</Display>
+      </Container>
+    </CenteredDisplay>
   );
 };
